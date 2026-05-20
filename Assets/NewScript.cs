57fc289a7ffa9895257a3a5c3dcd8211fe0ee0a9
Zohaib UR Rehman <100087461+zohaibJR@users.noreply.github.com>
2026-05-20 13:56:21 +0500

    Done with Basic Movement
    Done with Basic Movement

---
+++ Assets/NewScript.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class NewScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("GitHub Pushed");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
